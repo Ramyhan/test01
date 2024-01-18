9f090e9039b8c937cefc79b165a9781bbe6d7df1
kdw <KH302@DESKTOP-DDE5K7J>
2024-01-18 17:07:24 +0900

    동위 컨트롤러

---
+++ src/main/java/com/kh/test/controller/DongWiController.java
@@ -0,0 +1,13 @@
+package com.kh.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DongWiController {
+	
+	@GetMapping("/DongWi")
+	public void main() {
+		
+	}
+}
